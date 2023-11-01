2d699438ae7ecdf5cb066aba4817eae8435495f1
peter.yudhistiraa@gmail.com <peter.yudhistiraa@gmail.com>
2023-11-01 23:23:42 +0700

    [personal branch] tried adding a new class MahasiswaBIN.java for binary files.

--- src/daos/MahasiswaTXT.java
+++ src/daos/MahasiswaTXT.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import models.Mahasiswa;
 
 public class MahasiswaTXT implements MahasiswaDAO {
